1b7f1663b7da98420c18a8440118112564faf9bb
Your Name <you@example.com>
2023-03-29 12:03:23 +0100

    formatting

--- PythonApplication3/PythonApplication3.py
+++ PythonApplication3/PythonApplication3.py
@@ -1,4 +1,3 @@
-
 side1 = int(input("Enter length of first side of rectangle: "))
 side2 = int(input("Enter length of second side of rectangle: "))
 perimeter = 2*(side1 + side2)
@@ -6,7 +5,3 @@ area = side1 * side2
 
 print("The perimeter of rectangle is: " + str(perimeter))
 print("The area of rectangle is: " + str(area))
-
-
-
-
