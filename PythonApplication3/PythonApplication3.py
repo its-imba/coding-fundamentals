34155908be8e6bc08a2726ad7937f095a8260fe4
Your Name <you@example.com>
2023-03-29 11:43:14 +0100

    Add project files.

---
+++ PythonApplication3/PythonApplication3.py
@@ -0,0 +1,12 @@
+
+side1 = int(input("Enter length of first side of rectangle: "))
+side2 = int(input("Enter length of second side of rectangle: "))
+perimeter = 2*(side1 + side2)
+area = side1 * side2
+
+print("The perimeter of rectangle is: " + str(perimeter))
+print("The area of rectangle is: " + str(area))
+
+
+
+
