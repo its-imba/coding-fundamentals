2091c76eb4beb316d14e240c3b1d7767c062ea97
its-imba <its.sam.imba@gmail.com>
2023-03-29 14:43:55 +0100

    add part 1 code

---
+++ selectionp1.py
@@ -0,0 +1,11 @@
+person = int(input("Type in person's age: "))
+
+if person >= 18:
+    print("You are in category A")
+    
+elif person >= 16:
+    print("You are in category B")
+    
+else:
+    print("You are in category C")
+    
--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print("hello world")
+#This line starts with a # which means it is a comment line
+
+
+#Data types
+
+integer = 1010101
+string = str(1010101)
+boolean = True
+input = input("type the input: ")
+
+print("hello world", integer, string)
+
+print(input)
+
