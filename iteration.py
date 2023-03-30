5b66e1711a430cc138c625a487a03a6695e1c2f4
its-imba <its.sam.imba@gmail.com>
2023-03-30 11:35:59 +0100

    add counter

---
+++ iteration.py
@@ -0,0 +1,18 @@
+
+counter = 1
+
+while counter < 100:
+    
+    squared = counter * counter
+    print("the square of", counter, "is", squared)
+    counter += 1
+    
+    if squared >= 2000:
+        break
+    
+
+    
+
+    
+
+    
