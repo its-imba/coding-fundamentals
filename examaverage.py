1d1705fe2d31d644ef53589554fecb064e005167
its-imba <its.sam.imba@gmail.com>
2023-03-30 16:00:59 +0100

    add exam average

---
+++ examaverage.py
@@ -0,0 +1,11 @@
+mark1 = float(input("Enter first mark: "))
+mark2 = float(input("Enter second mark: "))
+mark3 = float(input("Enter third mark: "))
+
+averageMark =  (mark1 + mark2 + mark3) / 3
+
+if averageMark >= 65:
+    print("Pass")
+
+else:
+    print("Fail")
