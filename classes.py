fbd47965b6554d40cca714712b3bbe78b3495b4c
its-imba <its.sam.imba@gmail.com>
2023-04-03 12:06:05 +0100

    improve average calc & test scores method

--- classes.py
+++ classes.py
@@ -1,27 +1,29 @@
+import statistics
+
 # Create the class
 class Student:
     def __init__(self, name, age, currentClass):
         self.name = name
         self.age = age
         self.currentClass = currentClass
-        self.test_scores = []
+        self.testScores = []
 
-    # Accept a test score and add it to the test_scores list
-    def add_test_score(self, score):
-        self.test_scores.append(score)
+    # Accept test scores and add to list by using extention (in case there are already scores in list)
+    def addTestScores(self, score):
+        self.testScores.extend(score)
 
-    # Return the average of the test scores
-    def average_test_score(self):
-        return sum(self.test_scores) / len(self.test_scores)
+    # Return average of test scores using mean function (clearer/quicker way of calculating average instead of manually using sum & len)
+    def averageTestScore(self):
+        return statistics.mean(self.testScores)
         
-# Create a student object and add test scores
+# Create a student object and add test scores on one line
 student1 = Student("John", 20, "Science")
-student1.add_test_score(75)
-student1.add_test_score(85)
-student1.add_test_score(90)
+student1.addTestScores([55, 70, 85, 90])
 
 # Calculate the average test score
-average_score = student1.average_test_score()
+averageScore = student1.averageTestScore()
 
 # Print the average test score
-print(f"The average test score of {student1.name} in {student1.currentClass} is {average_score:.2f}")
+print(f"The average test score of {student1.name} in {student1.currentClass} is {averageScore:.2f}")
+
+# Prints: "The average test score of John in Science is 75.00"
