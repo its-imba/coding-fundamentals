7248fbe785b530360ca58be3e180c71f5f948dbe
its-imba <its.sam.imba@gmail.com>
2023-04-03 11:50:46 +0100

    modify comments

--- classes.py
+++ classes.py
@@ -6,20 +6,22 @@ class Student:
         self.currentClass = currentClass
         self.test_scores = []
 
+    # Accept a test score and add it to the test_scores list
     def add_test_score(self, score):
         self.test_scores.append(score)
 
+    # Return the average of the test scores
     def average_test_score(self):
         return sum(self.test_scores) / len(self.test_scores)
         
-# Creating a student object and adding test scores
+# Create a student object and add test scores
 student1 = Student("John", 20, "Science")
 student1.add_test_score(75)
 student1.add_test_score(85)
 student1.add_test_score(90)
 
-# Calculating the average test score
+# Calculate the average test score
 average_score = student1.average_test_score()
 
-# Printing the average test score
+# Print the average test score
 print(f"The average test score of {student1.name} in {student1.currentClass} is {average_score:.2f}")
